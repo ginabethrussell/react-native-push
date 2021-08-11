8a359f68f8587593e4e787aadb0989465e779cdc
ginabethrussell <ginabeth.russell@gmail.com>
2021-08-11 09:44:34 -0400

    sample app to demo push notifications with expo/react-native

--- App.js
+++ App.js
@@ -1,11 +1,111 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View, Button } from "react-native";
+import * as Notifications from "expo-notifications";
+import Constants from "expo-constants";
+
+// create a function to determine how notifications should be handled if the app is open
+// executed to tell OS how to handle an incoming notification
+// needs to return a promise
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldShowAlert: true,
+    };
+  },
+});
 
 export default function App() {
+  const [expoPushToken, setExpoPushToken] = React.useState("");
+  React.useEffect(() => {
+    registerForPushNotificationsAsync().then(
+      (token) => setExpoPushToken(token)
+      // send the token to your server, fetch('Your Api', token)
+    );
+  }, []);
+
+  React.useEffect(() => {
+    //listener when app is open
+    const foregroundSubscription =
+      Notifications.addNotificationReceivedListener((notification) => {
+        console.log(notification);
+      });
+    //listener when app is in the background
+    const backgroundSubscription =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+    return () => {
+      foregroundSubscription.remove();
+      backgroundSubscription.remove();
+    };
+  }, []);
+
+  const registerForPushNotificationsAsync = async () => {
+    if (Constants.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        alert("Failed to get push token for push notification!");
+        return;
+      }
+      const token = (await Notifications.getExpoPushTokenAsync()).data;
+      return token;
+    } else {
+      alert("Must use physical device for Push Notifications");
+    }
+    if (Platform.OS === "android") {
+      Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+  };
+
+  const triggerNotificationHandler = () => {
+    //always create a local notification
+    // Notifications.scheduleNotificationAsync({
+    //   content: {
+    //     title: "My first local notification",
+    //     body: "This is the first local notification we are sending!",
+    //   },
+    //   trigger: {
+    //     seconds: 10,
+    //   },
+    // });
+
+    // send a HTTP request to Expo server to send a notification
+    if (expoPushToken) {
+      fetch("https://exp.host/--/api/v2/push/send", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Accept-Encoding": "gzip, deflate",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          to: expoPushToken,
+          data: { extraData: "some data" },
+          title: "sent via the app",
+          body: "This push notification was sent from my app",
+        }),
+      });
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="Trigger Notification"
+        onPress={triggerNotificationHandler}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +114,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
